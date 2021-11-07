b8f4aab885b87e0869d10b9dd6723bbd38c8668f
oleg.shalaevsky <shaprj@gmail.com>
2021-11-08 02:41:22 +0300

    Removed legacy

--- src/main/kotlin/com/shaprj/test/recruitment/recruitment/data/repo/RecruiterRepo.kt
+++
@@ -1,7 +0,0 @@
-package com.shaprj.test.recruitment.recruitment.data.repo
-
-import com.shaprj.test.recruitment.recruitment.data.model.Recruiter
-import org.springframework.data.repository.CrudRepository
-import java.util.*
-
-interface RecruiterRepo : CrudRepository<Recruiter, UUID>
